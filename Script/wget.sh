99cc1bfefedcebbcf337261f0214e2f880b970b7
summer <superLeafxu@qq.com>
2020-02-18 23:17:58 +0800

    1. Something about UML.

--- Script/wget.sh
+++ Script/wget.sh
@@ -7,17 +7,18 @@ start=$3
 end=$4
 fileSurfix=$5
 storageDir=$6
+mode=$7
 
 check()
 {
     if [ "$url" == "" ]; then
-        echo "Format : download.sh [url] [prefix] [start] [end] [surfix]"
+        echo "=  Format : download.sh [url] [prefix] [start] [end] [surfix] [video|pic]"
         echo "Example: ./download.sh http://www.baidu.com/img/ baidu_resultlogo@ 0 2 png dir2"
         echo "Example: ./download.sh http://www.baidu.com/img/ '' 0 2 png dir1"
     	exit
 	else 
 		echo "========================================================="
-        echo "=  Format : download.sh [url] [prefix] [start] [end] [surfix]"
+        echo "=  Format : download.sh [url] [prefix] [start] [end] [surfix] [video|pic]"
 		echo "=  Url    : [$url]"
 		echo "=  Files  : $filePrefix[$start-$end].$fileSurfix"
 		echo "========================================================="
@@ -30,7 +31,7 @@ download()
 	echo
 	echo "Downloading ..."
 	# targetFile=$url$filePrefix$i.$fileSurfix
-	targetDir=`pwd`/$storageDir/$fileSurfix/
+	targetDir=`pwd`/$storageDir/
 
 	if [ ! -d $targetDir ]; then
 		echo "Target dir: "$targetDir
@@ -40,8 +41,14 @@ download()
 	
 	#len=`expr ${#end} \* 10`
 	#eval echo `echo {$start..$end}`
+	if [ "$mode" == "video" ]; then
+		arr=`seq -f "%0${#end}g" $start $end`
+	else 
+		arr=`seq $start $end`
+	fi
 
-	for i in `seq -f "%0${#end}g" $start $end`
+	for i in $arr
+	#for i in `seq -f "%0${#end}g" $start $end`
 	#for i in `seq $start $end`
 	do
 		targetFile=$targetDir$filePrefix$i.$fileSurfix
@@ -60,25 +67,28 @@ statusReport()
 {
 	# Check
 	total=0
-	for i in `seq $start $end`
+	rm  $targetDir/FileList
+	rm  $targetDir/missList
+	#for i in `seq $start $end`
+	for i in `seq -f "%0${#end}g" $start $end`
 	do
 		targetFile=$targetDir$filePrefix$i.$fileSurfix
 		if [ -f $targetFile ]; then
 			total=$[$total+1]	
 			successlog $targetFile
 		else
-			faillog "wget -q -P $targetDir $url$filePrefix$i.$fileSurfix"
+			faillog "$url$filePrefix$i.$fileSurfix"
 		fi
 	done
 	
 	all=`expr $end - $start + 1`
 	# mv $filePrefix*.$fileSurfix $targetDir
-	echo "All: $all Total download: $total Files"
+	echo "All: $all Total download: $total Files Dir:$targetDir"
 }
 
 successlog()
 {
-	echo $1 >> $targetDir/FileList
+	echo "file '$1'" >> $targetDir/FileList
 }
 
 faillog()
